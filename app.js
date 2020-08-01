4312a637bd629cd21e5802db85b72dc5d1afd677
Soufiane MOUSSAID <soufiane.moussaid@teal.ma>
2020-08-01 19:06:31 +0100

    add mongoose lib

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/covid19", { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.once("open", () => console.log("db is on"));
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Hi! Everyone"));
 
-app.listen(3000);
+app.listen(3000, console.log("Server is starting at port :", 3000));
