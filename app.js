0536511c29820a13cc9f8058a58e7c50a447828a
Soufiane MOUSSAID <soufiane.moussaid@teal.ma>
2020-08-02 15:34:03 +0100

    project structure

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ mongoose.connect("mongodb://localhost/covid19", { useNewUrlParser: true }, () =>
 
 const db = mongoose.connection;
 
+const app = express();
+
+// Only log error messages
 app.use(morgan('dev', {
     skip: function (req, res) { return res.statusCode < 400 }
 }));
 
-
-const app = express();
-
 app.get("/", (req, res) => res.send("Hi! Everyone"));
 
-app.listen(3000, console.log("Server is starting at port :", 3000));
+const port = process.env.PORT || 3000;
+
+app.listen(3000, console.log(`Server is starting at port 3000 ${port}`));
