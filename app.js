0ad1b406b867571f7f6b8a037d58c1a1236a97df
Soufiane MOUSSAID <soufiane.moussaid@teal.ma>
2020-08-07 17:54:01 +0100

    app.js

--- app.js
+++ app.js
@@ -28,4 +28,5 @@ app.use('/feedback', feedbackRoute);
 const port = process.env.PORT || CONSTANT.PORT;
 app.listen(CONSTANT.PORT, console.log(`Server is starting at port ${port}`));
 
-mongoose.connect('mongodb://localhost/covid19', {useNewUrlParser: true}, () => console.log('Connected to database'));
+mongoose.connect('mongodb://localhost/covid19',
+  {useNewUrlParser: true}, () => console.log('Connected to database'));
