55a14c82b0b324c551ae91057b48d1264a96b5f0
Soufiane MOUSSAID <soufiane.moussaid@teal.ma>
2020-08-24 17:51:15 +0100

    fix corona sort render

--- routes/corona.js
+++ routes/corona.js
@@ -17,7 +17,7 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
   try {
-    const savedUpdates = await corona.find();
+    const savedUpdates = await corona.find().sort({createAt: 1});
     res.status(200).json(savedUpdates);
   } catch (err) {
     res.json({message: err.message});
